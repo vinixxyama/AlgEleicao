7cdb2c67545f3cead9eebcd81e00b08298bc35fa
vinixxyama <vini_yamamoto@hotmail.com>
2018-11-15 23:57:14 -0200

    primeiro commit, funçoes sender, receiver e initiNo criadas, sockets feitos.

---
+++ eleicao.py
@@ -0,0 +1,65 @@
+# - Alunos:
+#       Henrique afundado Kodama    RA:
+#       Vinicius Yamamoto RA:490105
+#
+# - Disciplina:
+#       Sistemas Distribuidos
+#
+# - Professo:
+#       Fabio Verdi
+#
+# - UFSCar Sorocaba
+# ------ Libraries ------ #
+
+import config
+import mod
+import sys
+import socket
+import threading
+import struct
+import time
+from time import sleep
+from thread import *
+
+ip0 = "224.0.0.251"
+ip1 = "224.0.0.251"
+ip2 = "224.0.0.251"
+ip3 = "224.0.0.251"
+#sender
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+#receiver
+sock0 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock1 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock2 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock3 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock4 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock5 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock6 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock7 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock8 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock9 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+def InitNo0():
+
+def InitNo1():
+
+def InitNo2():
+
+def InitNo3():
+
+def InitNo4():
+
+def InitNo5():
+
+def InitNo6():
+
+def InitNo7():
+
+def InitNo8():
+
+def InitNo9():
+
+def sender():
+
+def receiver():
+    
